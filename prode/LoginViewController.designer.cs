6ab6147497977d59512e2055b57f2a6e6a5c0135
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2012-06-13 20:14:40 -0300

    initial commit

---
+++ prode/LoginViewController.designer.cs
@@ -0,0 +1,28 @@
+// WARNING
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace prode
+{
+	[Register ("LoginViewController")]
+	partial class LoginViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtUsername { get; set; }
+
+		[Action ("Login:")]
+		partial void Login (MonoTouch.UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (txtUsername != null) {
+				txtUsername.Dispose ();
+				txtUsername = null;
+			}
+		}
+	}
+}
