5de838f905f56927656e5a8588b57a2fc6aac5a4
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2012-06-22 17:27:24 -0300

    implement user account view

--- prode/UserViewController.cs
+++ prode/UserViewController.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using prode.domain;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using MonoTouch.Dialog;
 
 namespace prode
 {
@@ -26,14 +27,47 @@ namespace prode
 		{
 			base.ViewDidLoad ();
 			View.BackgroundColor = UIColor.FromPatternImage(UIImage.FromFile("Default.png"));
-			
-			var button = UIButton.FromType(UIButtonType.RoundedRect);
-			button.Frame = new RectangleF(10, 260, 300, 40);
-			button.SetTitle("Cerrar Sesion", UIControlState.Normal);
-			button.TouchUpInside += delegate(object sender, EventArgs e) {
+						
+			View.AddSubview(
+				new UILabel{
+					Text = AppManager.Current.Repository.User.FormattedName,
+					TextAlignment = UITextAlignment.Right,
+					Frame = new RectangleF(10,20,300,40),
+					TextColor = UIColor.White,
+					Font = UIFont.BoldSystemFontOfSize(24),
+					BackgroundColor = UIColor.Clear
+			});
+
+			View.AddSubview(
+				new UILabel{
+					Text = string.Format("({0})", AppManager.Current.Repository.User.NickName),
+					TextAlignment = UITextAlignment.Right,
+					Frame = new RectangleF(10,50,300,20),
+					TextColor = UIColor.White,
+					Font = UIFont.BoldSystemFontOfSize(14),
+					BackgroundColor = UIColor.Clear
+			});
+
+			var logoutButton = new GlassButton(new RectangleF (10, 310, 300, 40)) {
+     			NormalColor = UIColor.FromRGBA(222/255f, 222/255f, 225/255f, 0.25f),
+     			HighlightedColor = UIColor.Black
+ 			};
+			logoutButton.SetTitle("Cerrar Sesión", UIControlState.Normal);
+			logoutButton.Font = UIFont.BoldSystemFontOfSize(14);
+			logoutButton.Tapped += delegate {
 				AppManager.Current.Logout();
 			};
-			View.AddSubview(button);
+			View.AddSubview(logoutButton);
+			
+			View.AddSubview(
+				new UILabel{
+					Text = "Al cerrar la sesión, la próxima vez que abras esta aplicación, deberás volver a ingresar tu usuario y contraseña",
+					Frame = new RectangleF(15,340,290,80),
+					Lines = 3,
+					TextColor = UIColor.White,
+					Font = UIFont.SystemFontOfSize(12),
+					BackgroundColor = UIColor.Clear
+			});
 		}
 		
 		public override void ViewDidUnload ()
