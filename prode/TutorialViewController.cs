e0738c2ae946fdef50a89a5f6509a071f1f1e88b
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2012-08-11 20:58:30 -0300

    style up tutorial view

--- prode/TutorialViewController.cs
+++ prode/TutorialViewController.cs
@@ -23,63 +23,72 @@ namespace prode
 						
 			View.AddSubviews(
 				new UILabel{
-					Text = "Bienvenido a ComunidadProde!",
+					Text = "Bienvenido a ComunidadProde!!",
 					TextAlignment = UITextAlignment.Center,
-					Frame = new RectangleF(10,20,300,20),
-					Lines = 2,
+					Frame = new RectangleF(0,0,320,50),
+					Lines = 1,
 					TextColor = UIColor.White,
 					Font = UIFont.BoldSystemFontOfSize(18),
-					BackgroundColor = UIColor.Clear
+					BackgroundColor = UIColor.FromRGBA(222/255f, 222/255f, 225/255f, 0.25f)
 				},
 				new UILabel{
-					Text = "Para comenzar a jugar, simplemente andá a www.comunidadprode.com.ar, y elegí el torneo que mas te guste.",
+				Text = "Estas viendo esta guía porque vemos que aún no has comenzado a jugar.",
 					TextAlignment = UITextAlignment.Center,
-					Frame = new RectangleF(10,80,300,60),
-					Lines = 3,
-					TextColor = UIColor.White,
-					Font = UIFont.SystemFontOfSize(16),
-					BackgroundColor = UIColor.Clear
-				},
-				new UILabel{
-					Text = "Encotranos en las redes sociales, y compartí con nosotros tus ideas!",
-					TextAlignment = UITextAlignment.Center,
-					Frame = new RectangleF(10,160,300,40),
+					Frame = new RectangleF(10,60,300,40),
 					Lines = 2,
 					TextColor = UIColor.White,
-					Font = UIFont.SystemFontOfSize(16),
+					Font = UIFont.SystemFontOfSize(15),
+					BackgroundColor = UIColor.Clear
+				},
+				new UILabel{
+				Text = "Para ello, simplemente entrá en www.comunidadprode.com.ar, y dentro de 'Mis Tarjetas', guardá tu primer tarjeta en el torneo que desees",
+					TextAlignment = UITextAlignment.Center,
+					Frame = new RectangleF(10,120,300,80),
+					Lines = 4,
+					TextColor = UIColor.White,
+					Font = UIFont.SystemFontOfSize(15),
+					BackgroundColor = UIColor.Clear
+				},
+				new UILabel{
+					Text = "Si tenes preguntas, podes visitar la ayuda en la web, o bien contactarnos por las redes sociales!",
+					TextAlignment = UITextAlignment.Center,
+					Frame = new RectangleF(10,220,300,60),
+					Lines = 3,
+					TextColor = UIColor.White,
+					Font = UIFont.SystemFontOfSize(15),
 					BackgroundColor = UIColor.Clear
 				},
 				new UILabel{
 					Text = "@comunidadprode",
 					TextAlignment = UITextAlignment.Center,
-					Frame = new RectangleF(10,230,300,20),
+					Frame = new RectangleF(10,280,300,20),
 					TextColor = UIColor.White,
-					Font = UIFont.BoldSystemFontOfSize(18),
+					Font = UIFont.BoldSystemFontOfSize(12),
 					BackgroundColor = UIColor.Clear
 				},
 				new UILabel{
-					Text = "facebook.com/comunidadprode",
+					Text = "facebook.com/comuprode",
 					TextAlignment = UITextAlignment.Center,
-					Frame = new RectangleF(10,270,300,20),
+					Frame = new RectangleF(10,300,300,20),
 					TextColor = UIColor.White,
-					Font = UIFont.BoldSystemFontOfSize(18),
+					Font = UIFont.BoldSystemFontOfSize(12),
 					BackgroundColor = UIColor.Clear
 				},
 				new UILabel{
-					Text = "comunidadprode@gmail.com",
+					Text = "contacto@comunidadprode.com.ar",
 					TextAlignment = UITextAlignment.Center,
-					Frame = new RectangleF(10,310,300,20),
+					Frame = new RectangleF(10,320,300,20),
 					TextColor = UIColor.White,
-					Font = UIFont.BoldSystemFontOfSize(18),
+					Font = UIFont.BoldSystemFontOfSize(12),
 					BackgroundColor = UIColor.Clear
 				},
 				new UILabel{
-					Text = "No te olvides de avisarle a tus amigos!!",
+					Text = "Gracias por ser parte de la comunidad!!",
 					TextAlignment = UITextAlignment.Center,
-					Frame = new RectangleF(10,340,300,40),
+					Frame = new RectangleF(10,360,300,40),
 					Lines = 2,
 					TextColor = UIColor.White,
-					Font = UIFont.SystemFontOfSize(16),
+					Font = UIFont.BoldSystemFontOfSize(18),
 					BackgroundColor = UIColor.Clear
 				}
 			);
